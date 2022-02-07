8c858db70ba6263d85489bc3555683588ddd0552
sophiavanmegen <82518148+sophiavanmegen@users.noreply.github.com>
2022-02-07 23:34:22 +0100

    Update demostreamlit.py

--- demostreamlit.py
+++ demostreamlit.py
@@ -20,13 +20,3 @@ show_df = st.sidebar.checkbox("Show dataframe")
 if show_df:
     st.dataframe(df)
 
-numeric_columns = df.select_dtypes('number').columns.tolist()
-var_to_plot = st.sidebar.selectbox("Select a variable to explore", numeric_columns)
-
-st.subheader(f'Plot of the distribution of {var_to_plot} see below:')
-st.line_chart(df[var_to_plot])
-
-fig2 = df.plot(var_to_plot, kind='hist', title='Happiness Score and normalized GDP').get_figure()
-st.pyplot(fig2)
-
-st.write(alt.Chart(df).mark_circle().encode(x='Freedom', y='Trust', size='GDP per capita', color='GDP per capita', tooltip=['Freedom', 'Trust', 'GDP per capita']))
