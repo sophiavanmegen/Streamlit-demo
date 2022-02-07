64e8a7bc49143de17137fd547e01d92a0c194b11
sophiavanmegen <82518148+sophiavanmegen@users.noreply.github.com>
2022-02-07 23:35:27 +0100

    Update demostreamlit.py

--- demostreamlit.py
+++ demostreamlit.py
@@ -6,13 +6,13 @@ import altair as alt
 st.title("Hello World - Lesson 4")
 st.text("In the lession, we go through a demo of using the package streamlit to build webapps.")
 
-st.image('build.jpg')
+#st.image('build.jpg')
 
 st.sidebar.write('Please select your interests')
 
 st.write("This is some text")
 
-df = pd.read_csv('happiness_data_clean.csv')
+#df = pd.read_csv('happiness_data_clean.csv')
 #st.dataframe(df)
 
 show_df = st.sidebar.checkbox("Show dataframe")
