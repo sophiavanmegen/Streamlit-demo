67897b0b4a893f08d4db3e3be03f8009dfdc7425
sophiavanmegen <82518148+sophiavanmegen@users.noreply.github.com>
2022-02-15 19:34:58 +0100

    Update demostreamlit.py

--- demostreamlit.py
+++ demostreamlit.py
@@ -20,3 +20,41 @@ show_df = st.sidebar.checkbox("Show dataframe")
 if show_df:
     st.dataframe(df)
 
+
+    import streamlit as st
+from helpers import read_markdown
+
+import page_introduction as pi
+import page_distances as pd
+import page_documentation as doc
+
+
+# Page settings
+
+st.set_page_config(page_title='CosmΩracle')
+
+#Sidebar settings
+
+logo, name = st.sidebar.columns(2)
+with logo:
+    image = 'https://github.com/nikosarcevic/CosmOracle/blob/main/images/LogowName.png?raw=true'
+    st.image(image, use_column_width=True)
+
+st.sidebar.write(" ")
+
+pages = {
+        "Introduction": pi,
+        "Cosmological Distances": pd,
+        "Definitions": doc,
+    }
+
+st.sidebar.title("Main options")
+
+# Radio buttons to select desired option
+page = st.sidebar.radio("", tuple(pages.keys()))
+
+pages[page].show_page()
+        
+# About
+about = read_markdown("docs/markdown/about.md")
+st.sidebar.markdown(about)
