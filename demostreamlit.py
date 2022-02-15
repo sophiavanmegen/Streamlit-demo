92c7de5c9b509e2b501b53c099aae977ceda1550
sophiavanmegen <82518148+sophiavanmegen@users.noreply.github.com>
2022-02-15 19:41:42 +0100

    Update demostreamlit.py

--- demostreamlit.py
+++ demostreamlit.py
@@ -3,11 +3,6 @@ import pandas as pd
 import altair as alt
 
 
-import page_introduction as pi
-import page_distances as pagedistance
-import page_documentation as doc
-
-
 st.title("Hello World - Lesson 4")
 st.text("In the lession, we go through a demo of using the package streamlit to build webapps.")
 
@@ -26,34 +21,3 @@ if show_df:
     st.dataframe(df)
 
 
-
-
-# Page settings
-
-st.set_page_config(page_title='CosmΩracle')
-
-#Sidebar settings
-
-#logo, name = st.sidebar.columns(2)
-#with logo:
-#    image = 'https://github.com/nikosarcevic/CosmOracle/blob/main/images/LogowName.png?raw=true'
-#    st.image(image, use_column_width=True)
-
-st.sidebar.write(" ")
-
-pages = {
-        "Introduction": pi,
-        "Cosmological Distances": pd,
-        "Definitions": doc,
-    }
-
-st.sidebar.title("Main options")
-
-# Radio buttons to select desired option
-page = st.sidebar.radio("", tuple(pages.keys()))
-
-pages[page].show_page()
-        
-# About
-about = read_markdown("docs/markdown/about.md")
-st.sidebar.markdown(about)
